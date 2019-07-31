4d14002cecbe2ad4dc4b1fc0a77b7544ddf87a69
phansonloc1999 <phansonloc1999@gmail.com>
2019-07-31 11:04:40 +0700

    Clear completed rows

--- src/Scripts/Player.lua
+++ src/Scripts/Player.lua
@@ -5,6 +5,7 @@ PLAYZONE_WIDTH, PLAYZONE_HEIGHT = 210, 390
 PREVIEW_FRAME_WIDTH, PREVIEW_FRAME_HEIGHT = 120, 120
 CLEARED_BLOCK_SCORE = 10
 BLOCK_MATRIX_ROW, BLOCK_MATRIX_COLUMN = (PLAYZONE_HEIGHT + 120) / BLOCK_HEIGHT, PLAYZONE_WIDTH / BLOCK_WIDTH
+BLOCK_SCORE = 10
 
 function Player:init(playzoneX, playzoneY)
     --- Constants
@@ -58,6 +59,8 @@ end
 
 function Player:update(dt)
     if (self._activeTetromino:isStopped()) then
+        self:rowClearanceUpdate()
+
         self._nextTetromino:toSpawn(self._PLAYZONE_X, self._PLAYZONE_Y)
         self._activeTetromino = self._nextTetromino
         self._activeTetromino:getIndividualBlocks()
@@ -71,27 +74,7 @@ function Player:update(dt)
     else
         self:activeTetroFallUpdate()
         TETROMINO_FALL_TIMER = 1
-
-        --- If activeTetromino is obstructed, stop it
-        for i = 1, #self._activeTetromino._def do
-            for j = 1, #self._activeTetromino._def[i] do
-                if (self._activeTetromino._blocks[i][j]) then
-                    local row, column =
-                        self._activeTetromino._blocks[i][j]:getPosInMatrix(self._PLAYZONE_X, self._PLAYZONE_Y)
-                    if (row < BLOCK_MATRIX_ROW) then
-                        if (self._blocks[row + 1][column]) then
-                            if
-                                (self._activeTetromino._blocks[i][j]:isObstructedBy(self._blocks[row + 1][column]) and
-                                    self._activeTetromino ~= self._blocks[row + 1][column]._parentTetromino)
-                             then
-                                self._activeTetromino:stop()
-                                return
-                            end
-                        end
-                    end
-                end
-            end
-        end
+        self:checkActiveForObstruction()
     end
 
     self:tetrominoMovementUpdate(dt)
@@ -113,6 +96,7 @@ function Player:tetrominoMovementUpdate(dt)
             self._activeTetromino:getIndividualBlocks()
         end
         self:updateActiveBlocksInMatrix()
+        self:checkActiveForObstruction()
     end
     if (love.keyboard.wasPressed("left")) then
         self:removeActiveBlocksInMatrix()
@@ -124,6 +108,7 @@ function Player:tetrominoMovementUpdate(dt)
             self._activeTetromino:getIndividualBlocks()
         end
         self:updateActiveBlocksInMatrix()
+        self:checkActiveForObstruction()
     elseif (love.keyboard.wasPressed("right")) then
         self:removeActiveBlocksInMatrix()
         self._activeTetromino:moveRight()
@@ -134,6 +119,7 @@ function Player:tetrominoMovementUpdate(dt)
             self._activeTetromino:getIndividualBlocks()
         end
         self:updateActiveBlocksInMatrix()
+        self:checkActiveForObstruction()
     end
 
     if (love.keyboard.isDown("down")) then
@@ -145,14 +131,12 @@ function Player:verifyMovement()
     local currentBlock  ---@type Block
     local row, column
     for i = 1, #self._activeTetromino._def do
-        for j = 1, #self._activeTetromino._def[1] do
+        for j = 1, #self._activeTetromino._def[i] do
             if (self._activeTetromino._blocks[i][j]) then
                 currentBlock = self._activeTetromino._blocks[i][j]
                 if
                     (currentBlock._pos._x < self._PLAYZONE_X or
-                        currentBlock._pos._x > self._PLAYZONE_X + PLAYZONE_WIDTH - BLOCK_WIDTH) or
-                        (currentBlock._pos._y < self._PLAYZONE_Y or
-                            currentBlock._pos._y > self._PLAYZONE_Y + PLAYZONE_HEIGHT - BLOCK_HEIGHT)
+                        currentBlock._pos._x > self._PLAYZONE_X + PLAYZONE_WIDTH - BLOCK_WIDTH)
                  then
                     return "invalid"
                 end
@@ -200,6 +184,79 @@ function Player:removeActiveBlocksInMatrix()
                 local row, column =
                     self._activeTetromino._blocks[i][j]:getPosInMatrix(self._PLAYZONE_X, self._PLAYZONE_Y)
                 self._blocks[row][column] = nil
+            end
+        end
+    end
+end
+
+function Player:rowClearanceUpdate()
+    local rowsNewlyFilled = {}
+    for i = 1, #self._activeTetromino._def do
+        for j = 1, #self._activeTetromino._def[i] do
+            if (self._activeTetromino._blocks[i][j]) then
+                local row = self._activeTetromino._blocks[i][j]:getRowInMatrix(self._PLAYZONE_Y)
+                table.insert(rowsNewlyFilled, row)
+                break
+            end
+        end
+    end
+    local clearedRows = {}
+    if (#rowsNewlyFilled > 0) then
+        clearedRows = self:clearCompletedRows(rowsNewlyFilled)
+    end
+    for i = 1, #clearedRows do
+        for row = clearedRows[i] - 1, 1, -1 do
+            for column = 1, BLOCK_MATRIX_COLUMN do
+                if (self._blocks[row][column]) then
+                    self._blocks[row][column]:fall()
+                    local newRow, newColumn =
+                        self._blocks[row][column]:getPosInMatrix(self._PLAYZONE_X, self._PLAYZONE_Y)
+                    self._blocks[newRow][newColumn] = self._blocks[row][column]
+                    self._blocks[row][column] = nil
+                end
+            end
+        end
+    end
+end
+
+function Player:clearCompletedRows(rows)
+    local clearedRows = {}
+    for i = 1, #rows do
+        local blocksCounter = 0
+        for j = 1, BLOCK_MATRIX_COLUMN do
+            if (self._blocks[rows[i]][j]) then
+                blocksCounter = blocksCounter + 1
+            end
+        end
+        if (blocksCounter == BLOCK_MATRIX_COLUMN) then
+            table.insert(clearedRows, rows[i])
+            for j = 1, BLOCK_MATRIX_COLUMN do
+                self._blocks[rows[i]][j] = nil
+            end
+            self._score = self._score + BLOCK_SCORE * BLOCK_MATRIX_COLUMN
+        end
+    end
+    return clearedRows
+end
+
+function Player:checkActiveForObstruction()
+    --- If activeTetromino is obstructed, stop it
+    for i = 1, #self._activeTetromino._def do
+        for j = 1, #self._activeTetromino._def[i] do
+            if (self._activeTetromino._blocks[i][j]) then
+                local row, column =
+                    self._activeTetromino._blocks[i][j]:getPosInMatrix(self._PLAYZONE_X, self._PLAYZONE_Y)
+                if (row < BLOCK_MATRIX_ROW) then
+                    if (self._blocks[row + 1][column]) then
+                        if
+                            (self._activeTetromino._blocks[i][j]:isObstructedBy(self._blocks[row + 1][column]) and
+                                self._activeTetromino ~= self._blocks[row + 1][column]._parentTetromino)
+                         then
+                            self._activeTetromino:stop()
+                            return
+                        end
+                    end
+                end
             end
         end
     end
