ecdfeb615d3e353588c5185e7c55489abe4cb0d1
phansonloc1999 <phansonloc1999@gmail.com>
2019-08-19 08:25:53 +0700

    UI update for Menu state

--- src/Constants.lua
+++ src/Constants.lua
@@ -17,7 +17,37 @@ gTextures = {
         quit = {
             selected = love.graphics.newImage("assets/buttons/title screen/quit selected.png"),
             deselected = love.graphics.newImage("assets/buttons/title screen/quit deselected.png")
+        },
+        start = {
+            selected = love.graphics.newImage("assets/buttons/start selected.png"),
+            deselected = love.graphics.newImage("assets/buttons/start deselected.png")
+        },
+        about = {
+            selected = love.graphics.newImage("assets/buttons/about selected.png"),
+            deselected = love.graphics.newImage("assets/buttons/about deselected.png")
+        },
+        option = {
+            selected = love.graphics.newImage("assets/buttons/option selected.png"),
+            deselected = love.graphics.newImage("assets/buttons/option deselected.png")
+        },
+        menu_quit = {
+            selected = love.graphics.newImage("assets/buttons/quit selected.png"),
+            deselected = love.graphics.newImage("assets/buttons/quit deselected.png")
         }
     },
+    titles = {
+        menu = love.graphics.newImage("assets/titles/menu.png")
+    },
+    pet_emotes = {
+        pet1 = {
+            selected = love.graphics.newImage("assets/menu/pet 1 selected.png"),
+            deselected = love.graphics.newImage("assets/menu/pet 1 deselected.png")
+        },
+        pet2 = {
+            selected = love.graphics.newImage("assets/menu/pet 2 selected.png"),
+            deselected = love.graphics.newImage("assets/menu/pet 2 deselected.png")
+        }
+    },
+    ["background"] = love.graphics.newImage("assets/background.png"),
     ["title-screen"] = love.graphics.newImage("assets/title screen.png")
 }
--- src/GameStates/MenuState.lua
+++ src/GameStates/MenuState.lua
@@ -1,78 +1,161 @@
 ---@class MenuState
 MenuState = Class {__includes = BaseState}
 
-local BUTTON_WIDTH, BUTTON_HEIGHT = 100, 30
-
 function MenuState:init()
-    self._buttons = {} ---@type RectButton[]
-    self._buttons.playButton =
+    self._buttons = {}
+    local BUTTON_SPACING = 40
+
+    self._buttons.startButton =
         RectButton(
-        0,
-        0,
-        BUTTON_WIDTH,
-        BUTTON_HEIGHT,
+        WINDOW_WIDTH / 2 - gTextures.buttons.start.deselected:getWidth() / 2,
+        150,
         function()
             gStateMachine:change("select-mode")
         end,
-        nil,
-        nil
-    )
-    self._buttons.playButton:setPos(
-        getCenterX(0, love.graphics.getWidth(), self._buttons.playButton._hitbox._width),
-        250
+        {
+            selected = gTextures.buttons.start.selected,
+            deselected = gTextures.buttons.start.deselected
+        },
+        "deselected"
     )
 
     self._buttons.aboutButton =
         RectButton(
-        0,
-        0,
-        BUTTON_WIDTH,
-        BUTTON_HEIGHT,
+        WINDOW_WIDTH / 2 - gTextures.buttons.about.deselected:getWidth() / 2,
+        self._buttons.startButton:getY() + self._buttons.startButton:getHeight() + BUTTON_SPACING,
         function()
-        end
+        end,
+        {
+            selected = gTextures.buttons.about.selected,
+            deselected = gTextures.buttons.about.deselected
+        },
+        "deselected"
     )
-    self._buttons.aboutButton:setPos(
-        getPositionsWithOffsets(
-            self._buttons.playButton._hitbox._pos._x,
-            self._buttons.playButton._hitbox._pos._y,
-            0,
-            80
-        )
+
+    self._buttons.optionButton =
+        RectButton(
+        WINDOW_WIDTH / 2 - gTextures.buttons.option.deselected:getWidth() / 2,
+        self._buttons.aboutButton:getY() + self._buttons.aboutButton:getHeight() + BUTTON_SPACING,
+        function()
+            love.event.quit()
+        end,
+        {
+            selected = gTextures.buttons.option.selected,
+            deselected = gTextures.buttons.option.deselected
+        },
+        "deselected"
     )
 
     self._buttons.quitButton =
         RectButton(
-        0,
-        0,
-        BUTTON_WIDTH,
-        BUTTON_HEIGHT,
+        WINDOW_WIDTH / 2 - gTextures.buttons.menu_quit.deselected:getWidth() / 2,
+        self._buttons.optionButton:getY() + self._buttons.optionButton:getHeight() + BUTTON_SPACING,
         function()
             love.event.quit()
-        end
+        end,
+        {
+            selected = gTextures.buttons.menu_quit.selected,
+            deselected = gTextures.buttons.menu_quit.deselected
+        },
+        "deselected"
     )
-    self._buttons.quitButton:setPos(
-        getPositionsWithOffsets(
-            self._buttons.aboutButton._hitbox._pos._x,
-            self._buttons.aboutButton._hitbox._pos._y,
-            0,
-            80
-        )
+
+    local INDENT_TO_BUTTONS = 40
+
+    self._emotes = {pet1 = {}, pet2 = {}}
+
+    self._emotes.pet1[1] =
+        RectButton(
+        self._buttons.startButton:getX() + self._buttons.startButton:getWidth() + INDENT_TO_BUTTONS,
+        self._buttons.startButton:getY() + self._buttons.startButton:getHeight() / 2 -
+            gTextures.pet_emotes.pet1.deselected:getHeight() / 2,
+        function()
+        end,
+        {
+            deselected = gTextures.pet_emotes.pet1.deselected,
+            selected = gTextures.pet_emotes.pet1.selected
+        },
+        "deselected"
     )
+
+    self._emotes.pet1[2] =
+        RectButton(
+        self._buttons.optionButton:getX() + self._buttons.optionButton:getWidth() + INDENT_TO_BUTTONS,
+        self._buttons.optionButton:getY() + self._buttons.optionButton:getHeight() / 2 -
+            gTextures.pet_emotes.pet1.deselected:getHeight() / 2,
+        function()
+        end,
+        {
+            deselected = gTextures.pet_emotes.pet1.deselected,
+            selected = gTextures.pet_emotes.pet1.selected
+        },
+        "deselected"
+    )
+
+    self._emotes.pet2[1] =
+        RectButton(
+        self._buttons.aboutButton:getX() - INDENT_TO_BUTTONS - gTextures.pet_emotes.pet2.deselected:getWidth(),
+        self._buttons.aboutButton:getY() + self._buttons.aboutButton:getHeight() / 2 -
+            gTextures.pet_emotes.pet2.deselected:getHeight() / 2,
+        function()
+        end,
+        {
+            deselected = gTextures.pet_emotes.pet2.deselected,
+            selected = gTextures.pet_emotes.pet2.selected
+        },
+        "deselected"
+    )
+
+    print(gTextures.pet_emotes.pet2.deselected:getHeight())
+
+    self._emotes.pet2[2] =
+        RectButton(
+        self._buttons.quitButton:getX() - INDENT_TO_BUTTONS - gTextures.pet_emotes.pet2.deselected:getWidth(),
+        self._buttons.quitButton:getY() + self._buttons.quitButton:getHeight() / 2 -
+            gTextures.pet_emotes.pet2.deselected:getHeight() / 2,
+        function()
+        end,
+        {
+            deselected = gTextures.pet_emotes.pet2.deselected,
+            selected = gTextures.pet_emotes.pet2.selected
+        },
+        "deselected"
+    )
+
+    self._buttons.startButton._emoteMapping = self._emotes.pet1[1]
+    self._buttons.optionButton._emoteMapping = self._emotes.pet1[2]
+    self._buttons.aboutButton._emoteMapping = self._emotes.pet2[1]
+    self._buttons.quitButton._emoteMapping = self._emotes.pet2[2]
 end
 
 function MenuState:render()
+    love.graphics.draw(gTextures.background)
+
+    love.graphics.draw(gTextures.titles.menu, WINDOW_WIDTH / 2 - gTextures.titles.menu:getWidth() / 2, 30)
+
     for key, button in pairs(self._buttons) do
         button:render()
+    end
+
+    for key, petEmote in pairs(self._emotes) do
+        for i = 1, #petEmote do
+            petEmote[i]:render()
+        end
     end
 end
 
 function MenuState:update(dt)
-    if (love.mouse.wasPressed(1)) then
-        for key, button in pairs(self._buttons) do
-            if (button:collidesWithMouse()) then
+    for key, button in pairs(self._buttons) do
+        if (button:collidesWithMouse()) then
+            button:onSelect()
+            button._emoteMapping:onSelect()
+
+            if (love.mouse.wasPressed(1)) then
                 button:onClick()
-                return
             end
+        elseif (button._selected) then
+            button:onDeselect()
+            button._emoteMapping:onDeselect()
         end
     end
 end
