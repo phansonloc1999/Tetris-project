da7a0b3a7faaa1c419eda130958bb84ef81565e5
phansonloc1999 <phansonloc1999@gmail.com>
2019-10-18 21:15:35 +0700

    Remove Emmy DBG

--- main.lua
+++ main.lua
@@ -1,8 +1,3 @@
-package.cpath =
-    package.cpath .. ";c:/Users/COMPUTER/.vscode/extensions/tangzx.emmylua-0.3.28/debugger/emmy/windows/x64/?.dll"
-local dbg = require("emmy_core")
-dbg.tcpListen("localhost", 9966)
-
 require("src/Dependencies")
 
 function love.load()
